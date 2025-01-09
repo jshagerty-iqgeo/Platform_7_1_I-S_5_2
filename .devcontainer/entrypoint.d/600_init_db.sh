7427b4c2e56ce3437735e4c513138ff829cc9440
Justin Hagerty <jshagerty-iqgeo@iqgeo.com>
2025-01-09 16:04:43 -0500

    init

--- .devcontainer/entrypoint.d/600_init_db.sh
+++ .devcontainer/entrypoint.d/600_init_db.sh
@@ -4,4 +4,7 @@
 
 # START SECTION db init - if you edit these lines manually note that your change will get lost if you run the IQGeo Project Update tool
 if ! myw_db $MYW_DB_NAME list versions --layout keys | grep myw_comms_schema | grep version=; then myw_db $MYW_DB_NAME install comms; fi
+if ! myw_db $MYW_DB_NAME list versions --layout keys | grep mywapp_schema | grep version=; then myw_db $MYW_DB_NAME install mywapp_common; fi
+if ! myw_db $MYW_DB_NAME list versions --layout keys | grep groups | grep version=; then myw_db $MYW_DB_NAME install groups; fi
+if ! myw_db $MYW_DB_NAME list versions --layout keys | grep mywis_schema | grep version=; then myw_db $MYW_DB_NAME install survey; fi
 # END SECTION
